871cea7e2801f207fc1667447e5c0a467c1ca8d5
HOVOH <71901758+HOVOH@users.noreply.github.com>
2021-07-05 11:53:09 -0400

    increase wait time

--- src/twitter/twitter-sampling.service.ts
+++ src/twitter/twitter-sampling.service.ts
@@ -88,8 +88,8 @@ export class TwitterSamplingService {
       },
       (error) => this.logger.error("Stream error "+error.message),
       ()=> {
-        this.logger.log("Twitter sampling stream closed. Waiting 1sec before restart");
-        const timer = new Promise(res => setTimeout(res, 1000));
+        this.logger.log("Twitter sampling stream closed. Waiting 15 sec before restart");
+        const timer = new Promise(res => setTimeout(res, 15000));
         timer.then(() => this.startTweetSampling())
       }
     );
